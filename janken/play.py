33ad76a42f39a4e20faff2832af3c20fee711920
s16006 <s16006@std.it-college.ac.jp>
2016-08-07 12:15:09 +0900

    じゃんけん問題

--- janken/play.py
+++ janken/play.py
@@ -5,40 +5,45 @@ def select_hand():
     HANDS_choice = random.choice(HANDS)
     return HANDS_choice
 
-    """
-    コンピュータの手をランダムに決める
-    :return: HANDSの中のいずれか。
-    """
+
+    # コンピュータの手をランダムに決める
+    # :return: HANDSの中のいずれか。
+
 
 
 def judgement(player,  computer):
     if player == 'グー' and computer == 'チョキ':
-        return 1
+        return '勝ち'
     if player == 'チョキ'  and computer == 'パー':
-        return 1
+        return '勝ち'
     if  player == 'パー' and computer == 'グー':
-        return 1
+        return '勝ち'
 
     elif player == computer:
-        return 0
+        return 'あいこ'
     else:
-        return -1
+        return '負け'
 
 
 
 def save_score(result):
-    b = [{'win':'x', 'lose':'y', 'draw':'z'}]
-    f = open("score.py","w")
-    ff = b[result]
-    f.write(ff)
+    i = {'win':'勝ち', 'lose':'負け', 'draw':'あいこ'}
+    if result == '勝ち':
+        return 'win'
+    if result == '負け':
+        return 'lose'
+    if result == 'あいこ':
+        return 'draw'
+
+    f = open('Tutorial/janken/score.txt', 'w')
+    f.write(save_score(result))
     f.close
 
-"""
-    'score.txt'に戦績を保存。
-    win:x lose:y draw:zのディクショナリデータを保存する。
-    :param result:
-    :return: Non
-"""
+    # 'score.txt'に戦績を保存。
+    # win:x lose:y draw:zのディクショナリデータを保存する。
+    # :param result:
+    # :return: None
+
 
 
 if __name__ == '__main__':
@@ -47,7 +52,6 @@ if __name__ == '__main__':
     result = judgement(player, computer)
     # コンピュータの手と勝敗の結果を表示
 
-
+    save_score(result)
     print(result)
     print(player, computer)
-
